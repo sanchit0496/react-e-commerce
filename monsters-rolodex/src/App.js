dfe328c0e16fcba51252091d2c003c178bcb03b8
Sanchit <sanchit0496@gmail.com>
2021-09-27 14:59:26 +0530

    displaying names on frontend

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,44 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){ 
+    super(props)
+    this.state = {
+      monsters : [
+        {
+          name: 'Frank',
+          id: 'asc1'
+        }, 
+        {
+          name: 'Flintoff',
+          id: 'asc2'
+        },
+        {
+          name: 'Mark',
+          id: 'asc3'
+        },
+        {
+          name: 'Micheal',
+          id: 'asc4'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1 key = {monster.id}>{monster.name}</h1>
+          })
+        }      
+      </div>
+    )
+  }
 }
 
 export default App;
